ca516eb28c21332fbcce73ecd4ad1d80f6117acb
Luis Diego Hernandez <l.hernandezarias@metro-markets.de>
2022-02-04 20:05:36 +0100

    add endpoint for posts

---
+++ netlify/functions/get-posts.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const {
+  DATABASE_URL,
+  DATABASE_SECRET
+} = process.env;
+
+// Connect to our database 
+const { createClient } = require('@supabase/supabase-js');
+const supabase = createClient(DATABASE_URL, DATABASE_SECRET);
+
+// Our standard serverless handler function
+exports.handler = async event => {
+  const { data, error } = await supabase
+    .from('posts')
+    .select('*');
+
+  if (error) {
+    return {
+      statusCode: 500,
+      body: error,
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: data,
+  };
+};
